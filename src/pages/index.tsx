e3dabb03bc314ad45bbb569d69027bdf4257753e
Archknight23 <aerodonicus@protonmail.com>
2023-02-28 22:32:48 -0600

    On branch main  Changes to be committed: 	modified:   src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 /* Created by Chaos Foundry Tech Team : Site Modernization SOI group
 Est. 2020 - Created 02/27/2023
-Uses via Open Source M.I.T licences: MantineCSS, NextJS, WpGraphql, etc. (more to be added) 
+Use via Open Source M.I.T licences: MantineCSS, NextJS, WpGraphql, etc. (more to be added) 
 Deliverable Date: TBD - in progress*/
+
+/* Tasks for The Director and Director's Director for stream: 
+-App Shell Refactor
+-Drawer Design
+-Page Design
+ */
 
 import Head from 'next/head';
 import { MantineProvider, ColorSchemeProvider, ColorScheme, ActionIcon } from '@mantine/core';
